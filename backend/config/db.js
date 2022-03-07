c567a2cc765242286d975ed086488de312cf06b5
cituk <ecituk@thefives.com.mx>
2022-03-06 22:53:22 -0500

    Ocultando la conexión de la BD en Variables de Entorno

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const conectarDB = async () => {
     try {
-        const connection = await mongoose.connect("mongodb+srv://mern_user:EtemU9Z7W456fRzj@cluster0.lqwvr.mongodb.net/test?authSource=admin&replicaSet=atlas-gzube4-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",{
+        const connection = await mongoose.connect( process.env.MONGO_URI ,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
