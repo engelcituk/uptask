93db035693d35b4fa4c9f7a8886367183072a81d
cituk <ecituk@thefives.com.mx>
2022-03-09 00:06:11 -0500

    Validando el Token

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -77,7 +77,7 @@ const olvidePassword = async(req, res) => {
     const usuario = await Usuario.findOne({email})
     if( !usuario ){
         const error = new Error('Usuario no existe')
-        return res.status(400).json( { msg: error.message } )
+        return res.status(404).json( { msg: error.message } )
     }
 
     try {
@@ -89,9 +89,24 @@ const olvidePassword = async(req, res) => {
     }
 }
 
+const comprobarToken = async(req, res) => {
+    const { token } = req.params 
+    //Comprobar que usuario existe
+    const tokenValido = await Usuario.findOne({token})
+
+    if(tokenValido){
+        res.json( { msg:'Token válido y el usuario existe'} )
+
+    } else {
+        const error = new Error('Token no válido')
+        return res.status(404).json( { msg: error.message } )
+    }
+}
+
 export {
     registrar,
     autenticar,
     confirmar,
-    olvidePassword
+    olvidePassword,
+    comprobarToken
 }
--- backend/routes/usuarioRoutes.js
+++ backend/routes/usuarioRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express'
-import { registrar, autenticar, confirmar, olvidePassword } from '../controllers/usuarioController.js'
+import { registrar, autenticar, confirmar, olvidePassword, comprobarToken } from '../controllers/usuarioController.js'
 
 const router = express.Router()
 
 // Autenticación, Registro y confirmación de usuarios
 router.post('/', registrar) // crea un nuevo usuario
-router.post('/login', autenticar ) // crea un nuevo usuario
-router.get('/confirmar/:token', confirmar ) // crea un nuevo usuario
-router.post('/olvide-password', olvidePassword ) // crea un nuevo usuario
+router.post('/login', autenticar ) // autentica
+router.get('/confirmar/:token', confirmar ) // confirmar cuenta
+router.post('/olvide-password', olvidePassword ) // para solicitar un reseteo de password
+router.get('/olvide-password/:token', comprobarToken ) // recuperar cuenta
 
 export default router
