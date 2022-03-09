542932b6cf43f44b08e6c9ca5dbd06960172e42a
cituk <ecituk@thefives.com.mx>
2022-03-08 23:07:27 -0500

    ¿Qué es y como crear un JWT?

--- backend/controllers/usuarioController.js
+++ backend/controllers/usuarioController.js
@@ -1,5 +1,6 @@
 import Usuario from '../models/Usuario.js'
 import generarId from '../helpers/generarId.js'
+import generarJWT from '../helpers/generarJWT.js'
 
 const registrar = async (req, res) => {
     //evitar registros duplicados
@@ -39,7 +40,8 @@ const autenticar = async (req, res) => {
         res.json({
             _id: usuario._id,
             nombre: usuario.nombre,
-            email: usuario.email
+            email: usuario.email,
+            token: generarJWT( usuario._id )
         })
     } else {
         const error = new Error('La contraseña es incorrecta')
