08595318f60b2c8a213730785b2a0a231a8397b7
cituk <ecituk@thefives.com.mx>
2022-03-10 01:06:43 -0500

    Agregando Tareas a un Proyecto existente

--- backend/controllers/tareaController.js
+++ backend/controllers/tareaController.js
@@ -1,8 +1,31 @@
+import Proyecto from '../models/Proyecto.js'
 import Tarea from '../models/Tarea.js'
 
-
 const agregarTarea = async (req, res) => {
-    
+    const { proyecto } = req.body
+
+    if( proyecto.length != 24 ){
+        const error = new Error('el formato del id no es valido')
+        return res.status(400).json( { msg: error.message} )
+    }
+
+    const existeProyecto = await Proyecto.findById(proyecto)
+    if(!existeProyecto){
+        const error = new Error('El proyecto no existe')
+        return res.status(404).json( { msg: error.message } )
+    }
+
+    if( existeProyecto.creador.toString() !== req.usuario._id.toString() ){
+        const error = new Error('No puedes añadir tareas al proyecto si no eres creador')
+        return res.status(401).json( { msg: error.message} )
+    }
+
+    try {
+        const tareaAlmacenada = await Tarea.create( req.body )
+        return res.status(200).json( tareaAlmacenada )
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 const obtenerTarea = async (req, res) => {
