eb39f5dd1aa10b17d83bf8387dd8503d3c0e48cd
cituk <ecituk@thefives.com.mx>
2022-03-10 00:21:00 -0500

    Actualizar un Proyecto

--- backend/controllers/proyectoController.js
+++ backend/controllers/proyectoController.js
@@ -40,7 +40,39 @@ const obtenerProyecto = async (req, res) => {
 }
 
 const editarProyecto = async (req, res) => {
-  
+    
+    const { id } = req.params
+    const { nombre, cliente, descripcion,  fechaEntrega } = req.body
+    
+    if( id.length != 24 ){
+        const error = new Error('el formato del id no es valido')
+        return res.status(400).json( { msg: error.message} )
+    }
+
+    const proyecto = await Proyecto.findById(id)
+
+    if(!proyecto){
+        const error = new Error('El proyecto no existe')
+        return res.status(404).json( { msg: error.message } )
+    }
+
+    if( proyecto.creador.toString() !== req.usuario._id.toString() ){
+        const error = new Error('No puedes actualizar proyecto si no eres creador')
+        return res.status(401).json( { msg: error.message} )
+    }
+
+    proyecto.nombre = nombre || proyecto.nombre 
+    proyecto.cliente = cliente || proyecto.cliente 
+    proyecto.descripcion = descripcion || proyecto.descripcion 
+    proyecto.fechaEntrega = fechaEntrega || proyecto.fechaEntrega 
+    
+    try {
+        const proyectoActualizado = await proyecto.save()
+        return res.json(proyectoActualizado)
+    } catch (error) {
+        console.log(error)
+    }
+   
 }
 
 const eliminarProyecto = async (req, res) => {
