07746503d3eb1877e1ec398bc376538af146808a
cituk <ecituk@thefives.com.mx>
2022-03-11 00:32:48 -0500

    Creando el Proyecto

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+
+  return (
+    <div className="App">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt repellat ducimus labore eligendi? Exercitationem dolores temporibus minus mollitia reiciendis neque voluptatibus consectetur cupiditate provident tempora ratione, recusandae eos, consequatur officia.
+    </div>
+  )
+}
+
+export default App
+/**
+ * empezando a instalar dependencias para el front
+  npm create vite@latest
+  npm install    
+
+  Instalo axios y react-router-dom
+  npm i axios react-router-dom
+ */
