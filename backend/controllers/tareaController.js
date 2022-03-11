98c5a2c946b1850490ecd9db5eb3a5b312959893
cituk <ecituk@thefives.com.mx>
2022-03-10 23:52:49 -0500

    Actualizar una Tarea

--- backend/controllers/tareaController.js
+++ backend/controllers/tareaController.js
@@ -49,12 +49,41 @@ const obtenerTarea = async (req, res) => {
     }
 
     return res.json(tarea)
-
-
-
 }
 
 const actualizarTarea = async (req, res) => {
+    
+    const { id } = req.params
+    const { nombre, descripcion, prioridad, fechaEntrega } = req.body
+
+    if( id.length != 24 ){
+        const error = new Error('el formato del id no es valido')
+        return res.status(400).json( { msg: error.message} )
+    }
+
+    const tarea = await Tarea.findById(id).populate('proyecto')// traigo tambien el proyeccto asociado a este tarea
+
+    if(!tarea){
+        const error = new Error('La tarea no existe')
+        return res.status(404).json( { msg: error.message } )
+    }
+
+    if( tarea.proyecto.creador.toString() !== req.usuario._id.toString() ){
+        const error = new Error('No puedes ver esta tarea si no eres creador o colaborador')
+        return res.status(403).json( { msg: error.message} )
+    }
+
+    tarea.nombre = nombre || tarea.nombre 
+    tarea.descripcion = descripcion || tarea.descripcion 
+    tarea.prioridad = prioridad || tarea.prioridad 
+    tarea.fechaEntrega = fechaEntrega || tarea.fechaEntrega 
+
+    try {
+        const tareaActualizado = await tarea.save()
+        return res.json(tareaActualizado)
+    } catch (error) {
+        console.log(error)
+    }
     
 }
 
