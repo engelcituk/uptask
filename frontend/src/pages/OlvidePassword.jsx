697cada2fc7fd1149f2fdcf28abb91d8306fea4b
cituk <ecituk@thefives.com.mx>
2022-03-12 20:52:30 -0500

    Primeros pasos con Reestablecer Password

--- frontend/src/pages/OlvidePassword.jsx
+++ frontend/src/pages/OlvidePassword.jsx
@@ -1,12 +1,34 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import Alerta from '../components/Alerta'
 
 const OlvidePassword = () => {
+  const [ email, setEmail ] = useState('')
+  const [ alerta, setAlerta ] = useState({})
+  const { msg } = alerta
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    if (email === '' || email.length < 0 ) {
+      setAlerta({
+        msg: 'El campo email es obligatorio',
+        error: true
+      })
+      return
+    }
+  }
   return (
     <>
       <h1 className="text-sky-600 font-black text-6xl capitalize">Recupera tu {' '}
         <span className="text-slate-700">acceso</span>
       </h1>
-      <form action="" className="my-10 bg-white shadow rounded-lg p-10">
+      {
+        msg &&  <Alerta alerta={alerta}/>
+      }
+      <form
+        className="my-10 bg-white shadow rounded-lg p-10"
+        onSubmit={handleSubmit}
+      >
         <div className="my-5">
           <label
             htmlFor="email" 
@@ -17,6 +39,9 @@ const OlvidePassword = () => {
             type="email"
             placeholder="Email de registro"
             className="w-full mt-3 p-3 border rounded-lg bg-gray-50"
+            value={email}
+            onChange={ e => setEmail(e.target.value)}
+
           />
         </div>
 
