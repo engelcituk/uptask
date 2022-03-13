c0d0e101fe385f5832f41e137ecf4f56670034b7
cituk <ecituk@thefives.com.mx>
2022-03-12 19:48:43 -0500

    Enviando el Email de Bienvenida

--- backend/helpers/emails.js
+++ backend/helpers/emails.js
@@ -1,5 +1,28 @@
 import nodemailer from 'nodemailer'
 
-export const emailRegistro = ( datos ) => { //export con name
-  
+export const emailRegistro = async ( datos ) => { //export con name
+  const { email, nombre, token } = datos 
+
+  const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "8ebffee91228d3",
+      pass: "95dbf6bb50c42b"
+    }
+  })
+
+  //información del email
+  const info = await transport.sendMail({
+    from: '"upTask - Administrador de proyectos" <cuentas@uptask.com>',
+    to: email,
+    subject: 'UpTask - Comprueba tu cuenta',
+    text: 'Comprueba tu cuenta en UpTask',
+    html:`<p>Hola: ${nombre} Comprueba tu cuenta en UpTask.</p>
+      <p>Tu cuenta ya está casi lista, solo debes de comprobarla en el siguiente enlace:
+        <a href="${process.env.FRONTEND_URL}/confirmar/${token}">comprobar cuenta</a>
+      </p>
+      <p>Sí tu no creaste está cuenta, puedes ignorar este mensaje 😉</p>
+    `
+  })
 }
