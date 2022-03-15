c559bc6178c9070cdf4b3df6fb281e58c6d5cf45
cituk <ecituk@thefives.com.mx>
2022-03-15 01:29:36 -0500

    Solucionar el Detalle con la Fecha

--- frontend/src/helpers/formatearFecha.jsx
+++ frontend/src/helpers/formatearFecha.jsx
@@ -1,7 +1,7 @@
 
 export const formatearFecha = fecha => {
 
-    const nuevaFecha = new Date(fecha)
+    const nuevaFecha = new Date(fecha.split('T')[0].split('-'))
     const opciones = {
         weekday: 'long',
         year: 'numeric',
