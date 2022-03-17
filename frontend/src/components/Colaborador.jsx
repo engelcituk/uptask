fbbb9f488bff4dde1949e0117d0ba2f6025e9ce5
cituk <ecituk@thefives.com.mx>
2022-03-16 22:41:08 -0500

    Mostrando la Información de Colaboradores

--- frontend/src/components/Colaborador.jsx
+++ frontend/src/components/Colaborador.jsx
@@ -2,29 +2,19 @@ import { formatearFecha } from '../helpers/formatearFecha'
 import useProyectos from '../hooks/useProyectos'
 
 const Colaborador = ({colaborador}) => {
-    const { nombre, descripcion, fechaEntrega, prioridad, estado, _id } = colaborador
-    const { handleModalEditarTarea, handleModalEliminarTarea } = useProyectos()
+    const { nombre, email, _id } = colaborador
+    const { handleModalEditarTarea, handleModalEliminarColaborador } = useProyectos()
     
     return (
         <div className='border-b p-5 flex justify-between items-center'>
             <div>
                 <p className='mb-1 text-xl'>{nombre}</p>
-                <p className='mb-1 text-sm text-gray-500 uppercase'>{descripcion}</p>
-                {/* <p className='mb-1 text-sm'>{formatearFecha(fechaEntrega) }</p> */}
-                <p className='mb-1 text-gray-600'> Prioridad: {prioridad}</p>
-
-
+                <p className='mb-1 text-sm text-gray-500 uppercase'>{email}</p>
             </div>
             <div className="flex gap-2">
                 <button
-                    className="bg-indigo-600 px-4 py-3 text-white uppercase font-bold text-sm rounded-lg"
-                    onClick={ ()=> handleModalEditarTarea(colaborador) }
-                >Editar</button>
-               
-                
-                <button
                     className="bg-red-600 px-4 py-3 text-white uppercase font-bold text-sm rounded-lg"
-                    onClick={ () => handleModalEliminarTarea(colaborador)}
+                    onClick={ () => handleModalEliminarColaborador(colaborador)}
                 >Eliminar</button>
             </div>
         </div>
